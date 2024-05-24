2db618d80b1139838f3afcd9e7c6b593c3011a3d
akaufhold <info@stackfactory.de>
2024-05-24 13:18:10 +0200

    add new tca fields and localization

--- Build/Assets/Scripts/backend.js
+++ Build/Assets/Scripts/backend.js
@@ -3,20 +3,38 @@
 require(['TYPO3/CMS/Ajax/AjaxRequest'], function (AjaxRequest) {
   requirejs(['jquery'], function ($) {
     $(document).ready(() => {
-      // const sendToDalleButton = document.getElementsByClassName('sendToDalle')[0]
-      const imagePrompt = document.querySelectorAll('[data-formengine-input-name="data[tt_content][1][tx_dalleimage_prompt]"]')
-      document.getElementsByClassName('sendToDalle') &&
-      document.getElementsByClassName('sendToDalle')[0].addEventListener('click', () => {
-        const textPrompt = imagePrompt[0].value
-        const uid = urlParams.get('edit[tt_content]')
-        console.log(uid)
+      const getCurrentContentUid = () => {
+        let uid = 0
+        const queryString = window.location.search
+        const urlParams = new URLSearchParams(queryString)
+        urlParams.forEach((name, val) => {
+          console.log(name, val, name.includes('edit'))
+          name === 'edit' && (uid = val.split('edit[tt_content][')[1].split(']')[0])
+        })
+        return uid
+      }
+
+      const sendToDalleButton = document.getElementsByClassName('sendToDalle')
+      const inputName = '[data-formengine-input-name="data[tt_content][1][tx_dalleimage_prompt_'
+      const prompt = {
+        subject: document.querySelectorAll(`${inputName}_subject]"]`)[0],
+        description: document.querySelectorAll(`${inputName}_description]"]`)[0],
+        style: document.querySelectorAll(`${inputName}_style]"]`)[0],
+        colors: document.querySelectorAll(`${inputName}_colors]"]`)[0],
+        emotion: document.querySelectorAll(`${inputName}_emotion]"]`)[0],
+        composition: document.querySelectorAll(`${inputName}_composition]"]`)[0]
+      }
+
+      sendToDalleButton &&
+      sendToDalleButton[0].addEventListener('click', () => {
+        const textPrompt = prompt.description.value
         // Generate a random number between 1 and 32
         new AjaxRequest(TYPO3.settings.ajaxUrls.sf_dalleimages_getDalleImage)
-          .withQueryArguments({input: textPrompt, uid: uid})
+          .withQueryArguments({input: textPrompt, uid: getCurrentContentUid()})
           .get()
           .then(async function (response) {
-            const resolved = JSON.decode(await response.resolve())
-            console.log(resolved)
+            const resolved = await response.resolve()
+            resolved.result && location.reload()
           })
       })
     })
--- ext_tables.sql
+++ ext_tables.sql
@@ -1,3 +1,8 @@
 CREATE TABLE tt_content (
-	tx_dalleimage_prompt varchar(255) DEFAULT '' NOT NULL,
+	tx_dalleimage_prompt_subject varchar(255) DEFAULT '' NOT NULL,
+	tx_dalleimage_prompt_description varchar(255) DEFAULT '' NOT NULL,
+	tx_dalleimage_prompt_style varchar(255) DEFAULT '' NOT NULL,
+	tx_dalleimage_prompt_colors varchar(255) DEFAULT '' NOT NULL,
+	tx_dalleimage_prompt_emotion varchar(255) DEFAULT '' NOT NULL,
+	tx_dalleimage_prompt_composition varchar(255) DEFAULT '' NOT NULL,
 );
