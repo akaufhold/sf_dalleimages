afe4f7e10697423ee1892f4e62f0cf60a8e67be9
akaufhold <info@stackfactory.de>
2024-05-27 16:35:04 +0200

    refactor backend js

--- Build/Assets/Scripts/backend.js
+++ Build/Assets/Scripts/backend.js
@@ -1,71 +1,75 @@
 'use strict'
 /* eslint-disable no-undef */
-require(['TYPO3/CMS/Ajax/AjaxRequest'], function (AjaxRequest) {
+require(['TYPO3/CMS/Ajax/AjaxRequest', 'TYPO3/CMS/DocumentService'], function (AjaxRequest, DocumentService) {
   requirejs(['jquery'], function ($) {
-    $(document).ready(() => {
-      const getCurrentContentUid = () => {
-        let uid = 0
-        const queryString = window.location.search
-        const urlParams = new URLSearchParams(queryString)
-        urlParams.forEach((name, val) => {
-          console.log(name, val, name.includes('edit'))
-          name === 'edit' && (uid = val.split('edit[tt_content][')[1].split(']')[0])
-        })
-        return uid
-      }
+    DocumentService.ready().then(() => {
+      $(document).ajaxComplete(function () {
+        const generatePromptButton = document.getElementsByClassName('generatePrompt')
+        const sendToDalleButton = document.getElementsByClassName('sendToDalle')
+        const inputNamePrefix = '[name="data[tt_content][1][tx_dalleimage_prompt_'
+        const formEngineNamePrefix = '[data-formengine-input-name="data[tt_content][1][tx_dalleimage_prompt_'
+        const inputfieldList = 'colors,camera_position,subject,style,emotion,composition,artworks,artists,illustration,camera_position,camera_lenses,camera_shot,lighting,film_type,emotion,composition'
 
-      const sendToDalleButton = document.getElementsByClassName('sendToDalle')
-      const inputNamePrefix = '[data-formengine-input-name="data[tt_content][1][tx_dalleimage_prompt_'
-      const selectNamePrefix = '[name="data[tt_content][1][tx_dalleimage_prompt_'
+        // const selectfieldList = ''
 
-      const prompt = {
-        description: document.querySelector(`${inputNamePrefix}description]`).value,
-        subject: document.querySelector(`${inputNamePrefix}subject]`).value,
-        style: document.querySelector(`${selectNamePrefix}style]`).value,
-        colors: Array.from(document.querySelectorAll(`${selectNamePrefix}colors] option[selected]`), option => option.value),
-        emotion: Array.from(document.querySelectorAll(`${selectNamePrefix}emotion] option[selected]`), option => option.value),
-        composition: Array.from(document.querySelectorAll(`${selectNamePrefix}composition] option[selected]`), option => option.value),
-        artworks: document.querySelector(`${selectNamePrefix}artworks]`).value,
-        artists: document.querySelector(`${selectNamePrefix}artists]`).value,
-        camera_proximity: document.querySelector(`${selectNamePrefix}camera_proximity]`).value,
-        camera_position: document.querySelector(`${selectNamePrefix}camera_position]`).value,
-        camera_lenses: document.querySelector(`${selectNamePrefix}camera_lenses]`).value,
-        camera_shot: document.querySelector(`${selectNamePrefix}camera_shot]`).value,
-        lighting: document.querySelector(`${selectNamePrefix}lighting]`).value,
-        film_type: document.querySelector(`${selectNamePrefix}film_type]`).value
-      }
-
-      console.log(prompt)
-
-      const finalPrompt = `
-        ${(prompt.camera_shot !== '') ? `A ${prompt.camera_shot} of a ` : ''}
-        ${(prompt.colors.length > 0) ? `${prompt.colors.join(', ')} ` : ''}
-        ${(prompt.subject !== '') ? `${prompt.subject} ` : ''}
-        ${(prompt.style !== '') ? `in the style of ${prompt.style}, ` : ''}
-        ${(prompt.artworks !== '') ? `inspired by ${prompt.artworks} ` : ''}
-        ${(prompt.artists !== '') ? `created by ${prompt.artists}. ` : ''}
-        ${(prompt.emotion.length > 0) ? `This image should evoke a sense of ${prompt.emotion.join(', ')} and ` : ''}
-        ${(prompt.composition.length > 0) ? `be composed with ${prompt.composition.join(', ')}. ` : ''}
-        ${(prompt.camera_proximity !== '') ? `Capture it with a ${prompt.camera_proximity} ` : ''}
-        ${(prompt.camera_position !== '') ? `${prompt.camera_position} shot ` : ''}
-        ${(prompt.camera_lenses !== '') ? `using ${prompt.camera_lenses}. ` : ''}
-        ${(prompt.lighting !== '') ? `Illuminate with ${prompt.lighting} ` : ''}
-        ${(prompt.film_type !== '') ? `and consider using ${prompt.film_type} film for added effect.` : ''}
-      `
-
-      document.querySelector(`${inputName}description]`).value = finalPrompt
-
-      sendToDalleButton &&
-      sendToDalleButton[0].addEventListener('click', () => {
-        const textPrompt = prompt.description.value
-        // Generate a random number between 1 and 32
-        new AjaxRequest(TYPO3.settings.ajaxUrls.sf_dalleimages_getDalleImage)
-          .withQueryArguments({input: textPrompt, uid: getCurrentContentUid()})
-          .get()
-          .then(async function (response) {
-            const resolved = await response.resolve()
-            resolved.result && location.reload()
+        if (sendToDalleButton.length) {
+          const prompt = {}
+          inputfieldList.split(',').forEach((el) => {
+            prompt[el] = document.querySelector(`${inputNamePrefix}${el}]"]`).value.replaceAll(',', ', ')
+            require(['TYPO3/CMS/Event/RegularEvent'], function (RegularEvent) {
+              new RegularEvent('change', function (e) {
+                prompt[el] = document.querySelector(`${inputNamePrefix}${el}]"]`).value.replaceAll(',', ', ')
+                document.querySelector(`${inputNamePrefix}description]`).value = document.querySelector(`${formEngineNamePrefix}description]`).value = getFinalPrompt(prompt)
+              }).bindTo(document.querySelector(`${inputNamePrefix}${el}]"]`))
+            })
           })
+
+          const getFinalPrompt = (prompt) => {
+            return `${(prompt.illustration !== '') ? `A ${prompt.illustration} of a ` : 'A '}` +
+            `${(prompt.colors) ? `${prompt.colors} ` : ''}` +
+            `${(prompt.subject !== '') ? `${prompt.subject}` : ''}` +
+            `${(prompt.style !== '') ? ` in the style of ${prompt.style}. ` : '. '}` +
+            `${(prompt.artworks !== '') ? `Inspired by ${prompt.artworks}. ` : ''}` +
+            `${(prompt.artists !== '') ? `Created by ${prompt.artists}. ` : ''}` +
+            `${(prompt.emotion) ? `This image should evoke a sense of ${prompt.emotion}. ` : ''}` +
+            `${(prompt.composition) ? `It should be composed with ${prompt.composition}. ` : ''}` +
+            `${(prompt.camera_position !== '') ? `Capture it with a ${prompt.camera_position} ` : ''}` +
+            `${(prompt.camera_lenses !== '') ? `${prompt.camera_lenses} ` : ''}` +
+            `${(prompt.camera_shot !== '') ? `${prompt.camera_shot}. ` : '. '}` +
+            `${(prompt.lighting !== '') ? `Illuminate with ${prompt.lighting}. ` : ''}` +
+            `${(prompt.film_type !== '') ? `Consider using ${prompt.film_type} film for added effect.` : ''}`
+          }
+
+          /* eslint-disable no-unused-vars */
+          const getCurrentContentUid = () => {
+            let uid = 0
+            const queryString = window.location.search
+            const urlParams = new URLSearchParams(queryString)
+            urlParams.forEach((name, val) => {
+              name === 'edit' && (uid = val.split('edit[tt_content][')[1].split(']')[0])
+            })
+            return uid
+          }
+
+          require(['TYPO3/CMS/Event/RegularEvent'], function (RegularEvent) {
+            new RegularEvent('click', function (e) {
+              document.querySelector(`${inputNamePrefix}description]`).value = document.querySelector(`${formEngineNamePrefix}description]`).value = getFinalPrompt(prompt)
+            }).bindTo(generatePromptButton[0])
+
+            new RegularEvent('click', function (e) {
+              new AjaxRequest(TYPO3.settings.ajaxUrls.sf_dalleimages_getDalleImage)
+                .withQueryArguments({
+                  input: getFinalPrompt(prompt), 
+                  uid: getCurrentContentUid()
+                })
+                .get()
+                .then(async function (response) {
+                  const resolved = await response.resolve()
+                  resolved.result && location.reload()
+                })
+            }).bindTo(sendToDalleButton[0])
+          })
+        }
       })
     })
   })
