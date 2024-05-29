5a66e6e1389c7a3d8b0b4a951eef449b0404e3db
akaufhold <info@stackfactory.de>
2024-05-29 13:45:18 +0200

    add backend.css depending on typo3 version

--- ext_tables.php
+++ ext_tables.php
@@ -1,8 +1,20 @@
 <?php
 defined('TYPO3_MODE') || die();
 
+use TYPO3\CMS\Core\Information\Typo3Version;
+
 call_user_func(static function() {
 
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('sf_dalleimages', 'Configuration/TypoScript', 'Dalle Images');
 
+    if (TYPO3_MODE === 'BE') {
+        $typo3Version = new Typo3Version();
+        if ($typo3Version->getMajorVersion() > 11) {
+            $GLOBALS['TYPO3_CONF_VARS']['BE']['stylesheets']['sf_dalleimages'] = 'EXT:sf_dalleimages/Resources/Public/StyleSheets/';
+        } else {
+            $GLOBALS['TBE_STYLES']['skins']['sf_dalleimages'] = array();
+            $GLOBALS['TBE_STYLES']['skins']['sf_dalleimages']['name'] = 'Dalle Image Backend CSS';
+            $GLOBALS['TBE_STYLES']['skins']['sf_dalleimages']['stylesheetDirectories'][] = 'EXT:sf_dalleimages/Resources/Public/StyleSheets/';
+        }
+    }
 });
