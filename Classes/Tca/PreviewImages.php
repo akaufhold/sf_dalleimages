c68a3476d69b8a69874452084241c717e0215c1e
akaufhold <info@stackfactory.de>
2024-06-18 19:12:11 +0200

    add backend template for custom tca element

--- Classes/Tca/PreviewImages.php
+++ Classes/Tca/PreviewImages.php
@@ -15,83 +15,80 @@ use Stackfactory\SfDalleimages\Domain\Repository\AssetRepository;
 use TYPO3\CMS\Core\Resource\Exception\FileDoesNotExistException;
 use TYPO3\CMS\Core\Resource\File;
 use TYPO3\CMS\Core\Resource\ResourceFactory;
+use TYPO3\CMS\Core\Http\ServerRequestFactory;
+use TYPO3\CMS\Fluid\View\StandaloneView;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
+use TYPO3\CMS\Extbase\Configuration\BackendConfigurationManager;
+
+
+use TYPO3\CMS\Backend\Template\ModuleTemplate;
+use TYPO3\CMS\Backend\Template\ModuleTemplateFactory;
+
+use TYPO3\CMS\Core\Http\HtmlResponse;
+use TYPO3\CMS\Extbase\Mvc\ResponseInterface;
+use TYPO3\CMS\Core\Http\Response;
+use TYPO3\CMS\Core\Http\ResponseFactory;
+use TYPO3\CMS\Core\Http\StreamFactory;
+
 
 class PreviewImages extends AbstractNode implements NodeInterface
 {
-    /**
-     * Instance of the node factory to create sub elements, container and single element expansions.
-     *
-     * @var NodeFactory
-     */
-    protected $nodeFactory;
+    protected $view;
 
-    /**
-     * Renders the custom TCA field.
-     *
-     * @return array
-     */
+    public function __construct(NodeFactory $nodeFactory, array $data)
+    {
+        parent::__construct($nodeFactory, $data);
+    }
+
     public function render(): array
     {
+        // Initialize StandaloneView
+        $this->view = GeneralUtility::makeInstance(StandaloneView::class);
+
+        // Configure template path
+        $templatePath = 'EXT:sf_dalleimages/Resources/Private/Backend/Templates/';
+        $fluidTemplateFile = $templatePath . 'PreviewImages.html';
+        $this->view->setTemplatePathAndFilename($fluidTemplateFile);
+
+        // Fetch data and prepare variables for the template
         $record = $this->data['databaseRow'];
         $currentContentUid = $record['uid'];
-
         $this->imageService = GeneralUtility::makeInstance(ImageService::class);
-        getType($currentContentUid) === 'integer' && $assetUids = $this->imageService->getAssetsForContentElement('tt_content', $currentContentUid, 'crdate');
+        $assetUids = getType($currentContentUid) === 'integer' ? $this->imageService->getAssetsForContentElement('tt_content', $currentContentUid, 'crdate') : null;
 
-        // If there are no assets, return an empty string
+        // Process data and assign to the Fluid template
         if (!is_array($assetUids)) {
-            $html = '<div>No image found</div>';
+            $this->view->assign('fileReferences', []);
+            $isSliding = false; // Example assumption
         } else {
-            /** @var \Stackfactory\SfDalleimages\Domain\Repository\AssetRepository $assetRepository */
             $assetRepository = GeneralUtility::makeInstance(AssetRepository::class);
-            /** @var \TYPO3\CMS\Core\Resource\FileReference $fileReference */
             $fileReferences = $assetRepository->findByUidList($assetUids);
 
-            $elementsPerRow = 5;
-            $isSliding = (count($fileReferences) > $elementsPerRow) ? true : false;
-
-            // If the file reference is not found, return an empty string
             if (!$fileReferences) {
                 $html = '<div>No image found</div>';
             } else {
                 $this->resourceFactory = GeneralUtility::makeInstance(ResourceFactory ::class);
-                $html .= sprintf('<div id="carouselPreview" class="carousel carousel-dark dalle-preview slide '.($isSliding ? "px-5" : "").'" data-bs-ride="carousel" data-bs-wrap="false"><div class="carousel-inner row">');
-
                 foreach($fileReferences as $key => $fileReference) {
-                    ($key%$elementsPerRow === 0) && $html .= sprintf('<div class="dalle-preview-item carousel-item'.($key === 0 ?' active': '').($isSliding ? " py-4" : "").'">');
-    
                     $uid = $fileReference['uid'];
                     try {
                         /** @var File $file */
                         $file = $this->resourceFactory->getFileObject($uid);
                         $imageUrl = $file->getPublicUrl();
-                        $html .= sprintf('<img class="dalle-preview-item-image'.($isSliding ? " zoom p-2" : "").'" src="%s" alt="">', htmlspecialchars($imageUrl));
+                        $fileReferences[$key]['publicUrl'] = $imageUrl; 
                     } catch (FileDoesNotExistException $e) {
                         throw new \RuntimeException('Could not find files with the uid "' . $uid, 1314354065);
                     }
-                    ((($key)%$elementsPerRow === 4) || (count($fileReferences) === ($key+1))) && $html .= sprintf('</div>');
                 }
-                $html .= sprintf('</div>');
+            } 
 
-                if ($isSliding) {
-                    $html .= sprintf('
-                        <a class="carousel-control-prev" data-bs-target="#carouselPreview" data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="carousel-control-next" data-bs-target="#carouselPreview" data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                        </a></div></div>'
-                    );
-                } else {
-                    $html .= sprintf('</div>');
-                }
-            }
+            $this->view->assign('fileReferences', $fileReferences);
+
+            $elementsPerRow = 5;
+            $isSliding = count($fileReferences) > $elementsPerRow;
         }
+        $this->view->assign('isSliding', $isSliding);
+        $return['html'] = $this->view->render();
 
-        $result['html'] .= $html;
-
-		return $result;
+        return $return;
     }
 }
