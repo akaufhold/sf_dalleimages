f2d149ba66401840c44c1b46d0bae1a6ab052aa9
akaufhold <info@stackfactory.de>
2024-06-28 14:29:03 +0200

    [TASK] remove unsed js packages

--- Build/Assets/Scripts/sizeOptions.js
+++ Build/Assets/Scripts/sizeOptions.js
@@ -1,31 +1,28 @@
 import DocumentService from '@typo3/core/document-service.js'
-import $ from 'jquery'
 
 /* eslint-disable no-undef */
 DocumentService.ready().then(() => {
-  $(document).on('ajaxComplete', function () {
-    const modelField = document.querySelector('[name$="[tx_dalleimage_model]"]')
-    const sizeField = document.querySelector('[name$="[tx_dalleimage_size]"]')
+  const modelField = document.querySelector('[name$="[tx_dalleimage_model]"]')
+  const sizeField = document.querySelector('[name$="[tx_dalleimage_size]"]')
 
-    if (modelField && sizeField) {
-      modelField.addEventListener('change', function () {
-        const selectedModel = modelField.value
-        let defaultSize = ''
+  if (modelField && sizeField) {
+    modelField.addEventListener('change', function () {
+      const selectedModel = modelField.value
+      let defaultSize = ''
 
-        switch (selectedModel) {
-          case 'dall-e-1':
-            defaultSize = '256x256'
-            break
-          case 'dall-e-2':
-            defaultSize = '512x512'
-            break
-          case 'dall-e-3':
-            defaultSize = '1024x1024'
-            break
-        }
-        // Set the default value for the size field
-        sizeField.value = defaultSize
-      })
-    }
-  })
+      switch (selectedModel) {
+        case 'dall-e-1':
+          defaultSize = '256x256'
+          break
+        case 'dall-e-2':
+          defaultSize = '512x512'
+          break
+        case 'dall-e-3':
+          defaultSize = '1024x1024'
+          break
+      }
+      // Set the default value for the size field
+      sizeField.value = defaultSize
+    })
+  }
 })
