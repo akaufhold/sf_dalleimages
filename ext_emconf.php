12d434444e2f7819b1db70abd48023c62e166426
akaufhold <info@stackfactory.de>
2024-07-04 19:27:16 +0200

    [TASK] RELEASE 1.2.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,10 +18,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'info@stackfactory.de',
     'state' => 'alpha',
     'clearCacheOnLoad' => 0,
-    'version' => '1.1.1',
+    'version' => '1.2.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99',
+            'typo3' => '12.0.0-12.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],
